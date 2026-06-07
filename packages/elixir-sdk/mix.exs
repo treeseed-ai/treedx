a8d56382f4ce8945f951146fed36fd1ef9ddec0c
Adrian Webb <adrian@webb.sh>
2026-06-06 23:43:03 -0400

    build(sdk): bump version to 0.1.1

--- packages/elixir-sdk/mix.exs
+++ packages/elixir-sdk/mix.exs
@@ -4,7 +4,7 @@ defmodule TreeDxSdk.MixProject do
   def project do
     [
       app: :treedx,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: "Generic Elixir SDK for TreeDX.",
