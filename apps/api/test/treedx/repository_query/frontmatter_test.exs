112281795401b6aa5ce1317e9de9ff7cc8b245ea
Adrian Webb <adrian@webb.sh>
2026-09-15 09:58:56 -0400

    fix(query): preserve Unicode frontmatter strings

---
+++ apps/api/test/treedx/repository_query/frontmatter_test.exs
@@ -0,0 +1,18 @@
+defmodule TreeDx.RepositoryQuery.FrontmatterTest do
+  use ExUnit.Case, async: true
+
+  alias TreeDx.RepositoryQuery.Frontmatter
+
+  test "preserves printable Unicode frontmatter strings as strings" do
+    document = Frontmatter.parse("""
+    ---
+    rationale: "The SDK’s proposal can’t lose Unicode text."
+    ---
+
+    Body
+    """)
+
+    assert document.frontmatter["rationale"] == "The SDK’s proposal can’t lose Unicode text."
+    assert document.frontmatterError == nil
+  end
+end
